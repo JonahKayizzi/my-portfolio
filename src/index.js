25356f487501f55ef070c342246fe9bc59de5cad
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-03-10 14:50:22 +0300

    Add the store to the App

--- src/index.js
+++ src/index.js
@@ -2,20 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import LeftBar from './pages/LeftBar';
 import RightBar from './pages/RightBar';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <LeftBar />
-      <App />
-      <RightBar />
+      <Provider store={store}>
+        <LeftBar />
+        <App />
+        <RightBar />
+      </Provider>
     </Router>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
 
 reportWebVitals();
