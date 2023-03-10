a6eb8e35ced3c05df882ebffa773d8ed4b63fe1b
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-03-10 16:11:24 +0300

    Populate NavBar

--- src/pages/RightBar.js
+++ src/pages/RightBar.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const links = [
+  { id: 1, name: 'Home', path: '/' },
+  { id: 2, name: 'Projects', path: '/projects' },
+  { id: 3, name: 'Recommendations', path: '/recommendations' },
+  { id: 4, name: 'Employment', path: '/employment' },
+  { id: 5, name: 'Education', path: '/education' },
+  { id: 6, name: 'Publications', path: '/publications' },
+  { id: 7, name: 'Awards', path: '/awards' },
+];
 
 const RightBar = () => (
-  <div className="right-bar">
-    <div className="right-bar-header">
-      <h1>Right Bar</h1>
-    </div>
-    <div className="right-bar-body">
-      <p>Right Bar Body</p>
-    </div>
-  </div>
+  <nav className="right-bar">
+    <ul>
+      {links.map((link) => (
+        <li key={link.id}>
+          <NavLink to={link.path} activeClassName="active">
+            {link.name}
+          </NavLink>
+        </li>
+      ))}
+    </ul>
+  </nav>
 );
 
 export default RightBar;
