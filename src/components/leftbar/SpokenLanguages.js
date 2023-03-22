5aafccf255645ce8643c0a1a043901028b5faace
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-03-23 02:25:54 +0300

    Add styling to languages

--- src/components/leftbar/SpokenLanguages.js
+++ src/components/leftbar/SpokenLanguages.js
@@ -1,19 +1,28 @@
 import React from 'react';
 
 const SpokenLanguages = () => (
-  <ul className="languages">
-    <h4>Languages</h4>
-    <li>
-      <span>English</span>
-      <span>Native</span>
+  <ul className="flex-col px-3 py-2">
+    <h4 className="text-amber-400 text-sm font-bold my-2">Languages</h4>
+    <li className="flex-col text-xs pl-2 my-2">
+      <p className="flex justify-between">
+        <span>English</span>
+        <span>Native</span>
+      </p>
+      <div className="h-px bg-amber-400 w-full" />
     </li>
-    <li>
-      <span>Swahili</span>
-      <span>Intermediate</span>
+    <li className="flex-col text-xs pl-2 my-2">
+      <p className="flex justify-between">
+        <span>Swahili</span>
+        <span>Intermediate</span>
+      </p>
+      <div className="h-px bg-amber-400 w-1/2" />
     </li>
-    <li>
-      <span>German</span>
-      <span>Elementary</span>
+    <li className="flex-col text-xs pl-2 my-2">
+      <p className="flex justify-between">
+        <span>German</span>
+        <span>Elementary</span>
+      </p>
+      <div className="h-px bg-amber-400 w-1/5" />
     </li>
   </ul>
 );
