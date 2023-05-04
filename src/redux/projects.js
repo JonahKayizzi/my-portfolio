2adbd133fb86bcbc84ac4a0a9a2e4dfab3b29ca9
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-05-04 20:05:28 +0300

    Add more projects

--- src/redux/projects.js
+++ src/redux/projects.js
@@ -3,25 +3,6 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
   projects: [
     {
-      name: 'Air Navigation Services - Safety Management System',
-      rank: 1,
-      source: 'https://github.com/JonahKayizzi/ANS-SMS-APP',
-      images: ['images/SMS.jpg'],
-      tools: [
-        'PHP',
-        'Yii Framework',
-        'Java',
-        'React',
-        'microservices',
-        'Hibernate',
-        'CentOS',
-        'MySQL',
-        'Kanban',
-      ],
-      description:
-        'Upgrade and migration to Java. A web application for staff of ANS to report and analyze aircraft and other workplace incidents, occurrences, or hazards.',
-    },
-    {
       name: 'Doctor Reservations',
       rank: 2,
       source: 'https://github.com/JonahKayizzi/doctors-reservations-frontend',
@@ -43,23 +24,6 @@ const initialState = {
         'A responsive web application that enables users add add, remove and schedule appointments with doctors. The application runson two independent applications, a React front end that consumes API endpoints of a Rails backend',
     },
     {
-      name: 'Budget App',
-      rank: 3,
-      source: 'https://github.com/JonahKayizzi/Budget-App-RoR',
-      demo: 'https://budget-app-r5on.onrender.com/',
-      images: ['images/BUDGET.jpg'],
-      tools: [
-        'ERD',
-        'Ruby on Rails',
-        'PostgreSQL',
-        'ORM',
-        'Git & Gitflow',
-        'Devise',
-      ],
-      description:
-        'A mobile web application where you can manage your budget: you have a list of transactions associated with a category, so that you can see how much money you spent and on what',
-    },
-    {
       name: 'Space Travellers Hub',
       rank: 4,
       source: 'https://github.com/JonahKayizzi/space-travellers-hub',
@@ -75,6 +39,73 @@ const initialState = {
       ],
       description:
         'The Space Travellers hub is web application that works with real live data from the SpaceX API. The app is for a company that provides commercial and scientific space travel services. The application allows users to book rockets and join selected space missions.',
+    },
+    {
+      name: 'Bit Ticker',
+      rank: 5,
+      source: 'https://github.com/JonahKayizzi/BitTicker',
+      demo: 'https://roaring-swan-c9fce0.netlify.app/',
+      images: ['images/BitTickerMobile.png'],
+      tools: [
+        'REST API',
+        'React',
+        'Redux',
+        'Jest',
+        'React Testing Library',
+        'JavaScript',
+      ],
+      description:
+        'Your final stop for crypto statistics and analysis. BitTicker is a mobile web application that displays a list of metrics (crypto statistics) via a public API.',
+    },
+    {
+      name: 'Book Store',
+      rank: 6,
+      source: 'https://github.com/JonahKayizzi/bookstore-react-redux',
+      demo: 'https://stellular-torrone-7d081a.netlify.app/',
+      images: ['images/bookstore.png'],
+      tools: ['React', 'Redux', 'JavaScript', 'HTML5', 'CSS3'],
+      description:
+        'The Bookstore is a web application created with using MVP. The app displays a list of books, and the user can add a book, remove a selected book.',
+    },
+    {
+      name: 'To Do List',
+      rank: 7,
+      source: 'https://github.com/JonahKayizzi/To-do-app-with-React',
+      demo: 'https://incredible-alfajores-408a44.netlify.app/',
+      images: ['images/TODO.jpg'],
+      tools: ['React', 'Linters', 'JavaScript', 'Gitflow', 'HTML5', 'CSS3'],
+      description:
+        'This is a To do app developed using React & Lifecycle methods to consolidate knowledge about React, components, props, state, hooks and react router. A user can add, delete, updated and mark items as completed',
+    },
+    {
+      name: 'Math Magicians',
+      rank: 8,
+      source: 'https://github.com/JonahKayizzi/Calculator-App-React',
+      demo: 'https://teal-druid-4505fc.netlify.app/',
+      images: ['images/calculator.png'],
+      tools: ['React', 'Linters', 'JavaScript', 'Gitflow', 'HTML5', 'CSS3'],
+      description:
+        'Math magicians is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to: Make simple calculations. Read a random math-related quote.',
+    },
+    {
+      name: 'Leaderboard',
+      rank: 9,
+      source: 'https://github.com/JonahKayizzi/Leaderboard',
+      demo: 'https://jonahkayizzi.github.io/Leaderboard/dist/',
+      images: ['images/leaderboard.jpg'],
+      tools: [
+        'React',
+        'Linters',
+        'JavaScript',
+        'Gitflow',
+        'HTML5',
+        'CSS3',
+        'webpack',
+        'REST API',
+        'JSON',
+      ],
+      description:
+        'The Leaderboard App displays scores submitted by different players, allows you to submit your score and all data is preserved in the Leaderboard API service.',
     },
   ],
   searchedProjects: [],
