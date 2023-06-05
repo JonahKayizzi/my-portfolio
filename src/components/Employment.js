aea74980a394c3347f8f9f912e729e4dad6484f8
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-06-05 15:10:02 +0300

    Re-style work experience section

--- src/components/Employment.js
+++ src/components/Employment.js
@@ -4,38 +4,51 @@ import { Link } from 'react-router-dom';
 
 const History = () => {
   const { employments } = useSelector((state) => state.employmentsSl);
-  const [expandedEmployments, setExpandedEmployments] = useState([1]);
+  const [expandedEmploymentId, setExpandedEmploymentId] = useState(1);
 
   const toggleEmployment = (employmentId) => {
-    if (expandedEmployments.includes(employmentId)) {
-      setExpandedEmployments(
-        expandedEmployments.filter((id) => id !== employmentId)
-      );
+    if (employmentId === expandedEmploymentId) {
+      setExpandedEmploymentId(null);
     } else {
-      setExpandedEmployments([...expandedEmployments, employmentId]);
+      setExpandedEmploymentId(employmentId);
     }
   };
 
   return (
     <div className="flex flex-wrap justify-left gap-1 px-5">
-      <h3 className="py-2 font-extrabold place-self-start text-xl w-full bg-[rgb(25,25,25)]">
-        Where I have worked
+      <h3 className="py-2 flex font-extrabold place-self-start text-xl w-full bg-[rgb(25,25,25)]">
+        <span className="w-1/2">Where I have worked</span>
+        <span className="bottom-0 left-0 w-full h-0.5 bg-amber-500 mt-4" />
       </h3>
+
       {employments.map((employment) => (
         <div
           key={employment}
-          className="bg-[rgb(25,25,25)] font-thin lg:my-3 my-1 p-2 w-full border-l border-amber-400 flex"
+          className={`bg-[rgb(25,25,25)] font-thin lg:my-3 my-1 p-2 w-full ${
+            expandedEmploymentId === employment.id
+              ? 'border-l-2 border-amber-400'
+              : 'border-l-2 border-amber-500'
+          } flex`}
+          style={{
+            transition: 'height 1s ease-in-out',
+            height: expandedEmploymentId === employment.id ? 'auto' : '50px',
+            overflow: 'hidden',
+          }}
         >
           <h2 className="text-lg my-2 text-left ml-4 w-1/3">
             <button
-              className="text-amber-400 font-light"
+              className={`text-amber-400 font-light h-full w-11/12 p-2 ${
+                expandedEmploymentId === employment.id
+                  ? 'text-white bg-amber-400 transition duration-300 ease-in-out'
+                  : 'text-amber-500'
+              }`}
               type="button"
               onClick={() => toggleEmployment(employment.id)}
             >
               {employment.name}
             </button>
           </h2>
-          {expandedEmployments.includes(employment.id) && (
+          {expandedEmploymentId === employment.id && (
             <div className="text-gray-400 text-sm w-2/3">
               <p className="flex gap-2 text-lg text-white my-1">
                 <span className="font-bold">{employment.role}</span>
