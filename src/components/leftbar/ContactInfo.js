e39bc4bddfe9b8a84b3ce29a862ae881c732826e
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-06-08 11:27:48 +0300

    Add responsiveness to contact info

--- src/components/leftbar/ContactInfo.js
+++ src/components/leftbar/ContactInfo.js
@@ -12,25 +12,25 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 
 const ContactInfo = () => (
-  <div className="w-2/3 mb-4 flex flex-col items-left">
-    <p className="text-5xl">
+  <div className="lg:w-2/3 w-full mb-4 flex flex-col items-left">
+    <p className="text-5xl lg:text-left text-right">
       <span className="animate-fade-in-out delay-1000">✌️</span>
     </p>
-    <h1 className="font-bold text-5xl text-left my-6">
+    <h1 className="font-bold lg:text-5xl text-2xl text-left lg:my-6 my-2">
       <p>Hi, I am Jonathan Kayizzi</p>
       <p>
         <span className="text-amber-400 mr-2">Full Stack</span>
         <span>Engineer</span>
       </p>
     </h1>
-    <p className="text-md my-2 font-thin px-2 text-gray-400">
+    <p className="lg:text-md text-sm my-2 font-thin px-2 text-gray-400">
       Currently focussed on building innovative, scalable and maintainable
       applications using micro services architecture. Part-timing as a mentor
       for junior full-stack web developers at Microverse. And exploring database
       driven aeronautical chart production.
     </p>
-    <div className="w-4/5">
-      <ul className="flex justify-around gap-4 text-xs font-thin p-3 text-gray-400">
+    <div className="lg:w-4/5 w-full">
+      <ul className="flex justify-around lg:gap-4 gap-2 text-xs font-thin p-3 text-gray-400">
         <li className="">
           <p className="flex justify-end gap-3 my-2">
             <FontAwesomeIcon className="text-amber-400" icon={faHouse} />
@@ -51,7 +51,7 @@ const ContactInfo = () => (
         </li>
       </ul>
     </div>
-    <div className="flex justify-between text-black font-light text-xs my-4 w-2/3">
+    <div className="flex justify-between text-black font-light text-xs lg:my-4 my-2 lg:w-2/3 w-full">
       <Link
         target="_blank"
         to="mailto:jonahkayizzi@gmail.com?subject=Available for an opportunity?&body=Hi Jonathan,%20I%20would%20like%20to%20get%20to%20contact%20you%20regarding."
